1860a6353b453b911d6c9eb311b619c928e13ef7
Justin  Tucker <justin@certainstrings.com>
2016-08-09 15:53:53 -0700

    router now works

--- assets/js/image-provider-admin.js
+++ assets/js/image-provider-admin.js
@@ -3,6 +3,7 @@
     var media = wp.media,
         imageimplementor = imageimplementor || {},
         Library = media.controller.Library,
+        Select = media.view.MediaFrame.Select,
         Post = media.view.MediaFrame.Post;
 
     // extend the library controller for our insertion
@@ -15,10 +16,9 @@
             router: 'imageimplementor',
             toolbar: 'imageimplementor-toolbar',
             title: 'Image Implementor',
-            priority: 200,
+            priority: 400,
             searchable: false,
             sortable: false,
-            state: 'imageimplementor'
         }, Library.prototype.defaults),
 
         // this is fired when 'Add Media' is pressed
@@ -36,18 +36,28 @@
 
     });
 
-
     _.extend(media.view.MediaFrame.prototype, {
         ImageImplementor: {
 
             activate: function() {
-                this.content.mode('browse');
+                console.log( 'router on' );
+                var view = _.first(this.views.get('.media-frame-router')),
+                    viewSettings = {};
 
+                // console.log('activated');
 
+                viewSettings.upload_getty = {text: 'Getty Images', priority: 20};
+                viewSettings.upload_usat = {text: 'USA Today', priority: 40};
+
+                view.set(viewSettings);
+                this.content.mode('upload_getty');
             },
 
-            createToolbar: function(){
-                var controller = this;
+            deactivate: function () {
+                console.log('router off');
+            },
+
+            createToolbar: function () {
                 this.toolbar.set(new media.view.Toolbar({
                     controller: this,
                     items: {
@@ -55,30 +65,43 @@
                             style: 'primary',
                             text: 'Implement Image',
                             priority: 80,
-                            requires: {
-                                library: true,
-                                selection: true
-                            },
+                            requires: {selection: true},
 
+                            /**
+                             * @fires wp.media.controller.State#reset
+                             */
                             click: function () {
-                                var state = controller.state(),
-                                    selection = state.get('selection');
-                                //
-                                // controller.close();
-                                // state.trigger('videopress:insert', selection).reset();
+                                var controller = this.controller,
+                                    state = controller.state(),
+                                    edit = controller.state('gallery-edit');
+
+                                // edit.get('library').add(state.get('selection').models);
+                                // state.trigger('reset');
+                                // controller.setState('gallery-edit');
                             }
                         }
                     }
                 }));
             },
 
-
-
-
+            loadGetty: function () {
+                console.log( 'getty ready' );
+            },
         }
     });
 
-    media.view.MediaFrame.ImageImplementor = Post.extend({
+    // media.view.MediaFrame.Select = Select.extend({
+    //     bindHandlers: function () {
+    //         Select.prototype.bindHandlers.apply(this, arguments);
+    //     }
+    // });
+
+    media.view.MediaFrame.Post = Post.extend({
+
+        createStates: function () {
+            Post.prototype.createStates.apply(this, arguments);
+            this.states.add([new media.controller.ImageImplementor()]);
+        },
 
         bindHandlers: function () {
             Post.prototype.bindHandlers.apply(this, arguments);
@@ -87,32 +110,11 @@
             this.on('router:activate:imageimplementor', this.ImageImplementor.activate, this);
             this.on('router:deactivate:imageimplementor', this.ImageImplementor.deactivate, this);
 
-            this.on('toolbar:create:imageimplementor-toolbar', this.ImageImplementor.createToolbar, this);
-        },
-
-        createStates: function () {
-            Post.prototype.createStates.apply(this, arguments);
-            this.states.add([
-                new media.controller.ImageImplementor()
-            ]);
-        },
-
-        browseRouter: function (routerView) {
-
-            // routerView.set({
-            //     upload: {
-            //         text: 'UUUULLOOO',
-            //         priority: 20
-            //     },
-            //     browse: {
-            //         text: 'GET IMAGE',
-            //         priority: 40
-            //     }
-            // });
-
-        },
+            this.on('router:create:imageimplementor', this.createRouter, this);
+            this.on('toolbar:create:imageimplementor-toolbar', this.createToolbar, this);
+            this.on('content:render:upload_getty', this.loadGetty, this);
+        }
 
     });
-
 
 })(jQuery);
