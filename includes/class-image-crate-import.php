3cbd80cd44ca60a405381807bd3e970aab21cfe6
Justin  Tucker <justin@certainstrings.com>
2016-10-06 16:39:07 -0700

    Added filters for controlling image source display

--- includes/class-image-crate-import.php
+++ includes/class-image-crate-import.php
@@ -14,7 +14,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 final class Image_Crate_Import {
 
+	/**
+	 * Custom Directory for image storage
+	 * @var string
+	 */
 	private $directory;
+
+	/**
+	 * Get this party started
+	 *
+	 * todo: maybe fix collection not loading, unless attachments have been loaded
+	 * todo: filters below need to be fired earlier. i think --- LEFT OFF HERE
+	 */
+	public function __construct() {
+		/*
+		 * Update image source urls for images uploaded to the getty api
+		 */
+		add_filter( 'wp_prepare_attachment_for_js', array( $this, 'update_media_modal_file_refs' ), 99, 1 );
+		add_filter( 'wp_get_attachment_image_src', array( $this, 'set_image_path' ) );
+		add_filter( 'wp_calculate_image_srcset', array( $this, 'update_scrset_attr' ), 10, 1 );
+		add_filter( 'image_send_to_editor', array( $this, 'send_to_editor' ), 10, 1 );
+	}
 
 	/**
 	 * Import image from an url
@@ -214,4 +234,98 @@ final class Image_Crate_Import {
 		return $upload;
 
 	}
+
+	/**
+	 * Set up image path to point to new getty image location
+	 *
+	 * @param   array $response array of prepared attachment data.
+	 *
+	 * @return  string              array of updated attachment data.
+	 */
+	public function update_media_modal_file_refs( $response ) {
+
+		if ( preg_match( "/" . $this->directory . "/", $response['url'] ) ) {
+
+			$response['url'] = $this->set_image_path( $response['url'] );
+
+			if ( isset( $response['sizes'] ) ) {
+
+				foreach ( $response['sizes'] as $key => $size ) {
+					$response['sizes'][ $key ]['url'] = $this->set_image_path( $size['url'] );
+				}
+			}
+
+		}
+
+		return $response;
+	}
+
+	/**
+	 * Set up image path to point to new getty image location
+	 *
+	 * On the admin side, WordPress filters the output of the url string used to display the
+	 * image. That is altered here.
+	 *
+	 * @param   string|array $image_path Default image source
+	 *
+	 * @return  string|array                    Updated image src to include 'getty-images'
+	 */
+	public function set_image_path( $image_path ) {
+
+		// Make sure image path url is a string
+		if ( is_array( $image_path ) ) {
+			$image_path_url = $image_path[0];
+		} else {
+			$image_path_url = $image_path;
+		}
+
+		// Check for old multisite directory structure
+		if ( ! stristr( $image_path_url, '/files/' . $this->directory . '/' ) ) {
+			$search_str  = '/\/sites\/\d*\/' . $this->directory . '\//';
+			$replace_str = '/' . $this->directory . '/';
+		} else {
+			$search_str  = '/\/files\/' . $this->directory . '\//';
+			$replace_str = '/wp-content/uploads/' . $this->directory . '/';
+		}
+
+		if ( is_array( $image_path ) ) {
+			$image_path[0] = preg_replace( $search_str, $replace_str, $image_path[0] );
+		} else {
+			$image_path = preg_replace( $search_str, $replace_str, $image_path );
+		}
+
+		return $image_path;
+	}
+
+	/**
+	 * Update srcset urls to point to getty images global folder location
+	 *
+	 * @param   array $sources One or more arrays of source data to include in the 'srcset'.
+	 *
+	 * @return  array               Array with updated src in urls
+	 */
+	public function update_scrset_attr( $sources ) {
+
+		foreach ( $sources as $key => $source ) {
+			if ( preg_match( "/" . $this->directory . "/", $sources[ $key ]['url'] ) ) {
+				$sources[ $key ]['url'] = $this->set_image_path( $source['url'] );
+			}
+		}
+
+		return $sources;
+	}
+
+	/**
+	 * Update post meta and image path when image is sent to the editor
+	 * from the media modal.
+	 *
+	 * @param   string $html Image markup sent to the editor
+	 *
+	 * @return  string|array             Updated markup with 'getty-images' in src
+	 */
+	public function send_to_editor( $html ) {
+		return $this->set_image_path( $html );
+	}
+
+
 }
