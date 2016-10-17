739db7634e354aee01a81c820748719d046f5bba
Justin  Tucker <justin@certainstrings.com>
2016-10-16 20:27:43 -0700

    swap downloading folder location

--- includes/class-image-crate-api.php
+++ includes/class-image-crate-api.php
@@ -26,6 +26,11 @@ class Image_Crate_Api {
 		$this->secret = USAT_API_SECRET;
 		$this->api_url = "http://www.usatodaysportsimages.com/api/searchAPI/";
 		$this->directory = 'usat-images';
+
+		add_filter( 'wp_prepare_attachment_for_js', array( $this, 'update_media_modal_file_refs' ), 99, 1 );
+		add_filter( 'wp_get_attachment_image_src', array( $this, 'set_image_path' ) );
+		add_filter( 'wp_calculate_image_srcset', array( $this, 'update_scrset_attr' ), 10, 1 );
+		add_filter( 'image_send_to_editor', array( $this, 'send_to_editor' ), 10, 1 );
 	}
 
 	public function fetch( $phrase, $pageToLoad ) {
@@ -82,6 +87,7 @@ class Image_Crate_Api {
 
 		$response_body = json_decode(wp_remote_retrieve_body( $response ), true);
 
+		// todo: fix array error catching for secondary array_map param
 		$images = array_map( function ( $index ) {
 			return $index[0];
 		}, $response_body['results']['item'] );
@@ -119,5 +125,97 @@ class Image_Crate_Api {
 			'max_width' => $attachment['width'],
 			'max_height' => $attachment['height'],
 		);
+	}
+
+	/**
+	 * Set up image path to point to new getty image location
+	 *
+	 * @param   array $response array of prepared attachment data.
+	 *
+	 * @return  string              array of updated attachment data.
+	 */
+	public function update_media_modal_file_refs( $response ) {
+
+		if ( preg_match( "/" . $this->directory . "/", $response['url'] ) ) {
+
+			$response['url'] = $this->set_image_path( $response['url'] );
+
+			if ( isset( $response['sizes'] ) ) {
+
+				foreach ( $response['sizes'] as $key => $size ) {
+					$response['sizes'][ $key ]['url'] = $this->set_image_path( $size['url'] );
+				}
+			}
+
+		}
+
+		return $response;
+	}
+
+	/**
+	 * Set up image path to point to new getty image location
+	 *
+	 * On the admin side, WordPress filters the output of the url string used to display the
+	 * image. That is altered here.
+	 *
+	 * @param   string|array $image_path Default image source
+	 *
+	 * @return  string|array                    Updated image src to include 'getty-images'
+	 */
+	public function set_image_path( $image_path ) {
+
+		// Make sure image path url is a string
+		if ( is_array( $image_path ) ) {
+			$image_path_url = $image_path[0];
+		} else {
+			$image_path_url = $image_path;
+		}
+
+		// Check for old multisite directory structure
+		if ( ! stristr( $image_path_url, '/files/' . $this->directory . '/' ) ) {
+			$search_str  = '/\/sites\/\d*\/' . $this->directory . '\//';
+			$replace_str = '/' . $this->directory . '/';
+		} else {
+			$search_str  = '/\/files\/' . $this->directory . '\//';
+			$replace_str = '/wp-content/uploads/' . $this->directory . '/';
+		}
+
+		if ( is_array( $image_path ) ) {
+			$image_path[0] = preg_replace( $search_str, $replace_str, $image_path[0] );
+		} else {
+			$image_path = preg_replace( $search_str, $replace_str, $image_path );
+		}
+
+		return $image_path;
+	}
+
+	/**
+	 * Update srcset urls to point to getty images global folder location
+	 *
+	 * @param   array $sources One or more arrays of source data to include in the 'srcset'.
+	 *
+	 * @return  array               Array with updated src in urls
+	 */
+	public function update_scrset_attr( $sources ) {
+
+		foreach ( $sources as $key => $source ) {
+			if ( preg_match( "/" . $this->directory . "/", $sources[ $key ]['url'] ) ) {
+				$sources[ $key ]['url'] = $this->set_image_path( $source['url'] );
+			}
+		}
+
+		return $sources;
+	}
+
+	/**
+	 * Update post meta and image path when image is sent to the editor
+	 * from the media modal.
+	 *
+	 * @param   string $html Image markup sent to the editor
+	 *
+	 * @return  string|array             Updated markup with 'getty-images' in src
+	 */
+	public function send_to_editor( $html ) {
+		return $this->set_image_path( $html );
 	}
 }
